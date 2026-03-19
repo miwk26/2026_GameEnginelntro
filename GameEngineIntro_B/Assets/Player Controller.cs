646b2ed708bbb7090196df7c72abfdb14a6da30a
miwk26 <yshdjwu@gmail.com>
2026-03-19 15:44:52 +0900

    플레이어 점프 및 애니메이션

---
+++ GameEngineIntro_B/Assets/Player Controller.cs
@@ -0,0 +1,62 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.Translate(Vector3.right * moveInput.x * Time.deltaTime);
+        //transform.Translate(Vector3.up * moveInput.y * Time.deltaTime);
+
+        if (moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+        transform.Translate(Vector2.right * moveSpeed * moveInput.x * Time.deltaTime);
+    }
+
+    
+}
